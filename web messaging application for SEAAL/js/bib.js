3c31c4f8f601f66fdfebcebfe8b72e791582fff7
mounaToygar <fm_boutemine@esi.dz>
2019-08-20 23:48:48 +0100

    Add code files

---
+++ web messaging application for SEAAL/js/bib.js
@@ -0,0 +1,17 @@
+$(document).ready(function()
+{   
+	var ttc1=$("#ht1").value()+$("#ht1").value()*($("#ht1").value()/100);
+	$("#ttc1").text(ttc1);
+	var ttc2=$("#ht2").value()+$("#ht2").value()*($("#ht2").value()/100);
+	$("#ttc2").text(ttc2);
+	
+	var ttc3=$("#ht3").value()+$("#ht3").value()*($("#ht3").value()/100);
+	$("#ttc3").text(ttc3);
+	
+	var ttc4=$("#ht4").value()+$("#ht4").value()*($("#ht4").value()/100);
+	$("#ttc4").text(ttc4);
+	var ttc5=$("#ht5").value()+$("#ht5").value()*($("#ht5").value()/100);
+	$("#ttc5").text(ttc5);
+});
+
+
